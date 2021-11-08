f7eb6d1ccf5495bc3b909e0042e0a04cc4ab47db
Joona <joonajarasanen@gmail.com>
2021-11-08 09:26:55 +0200

    modified and added files

--- src/tests/varasto_test.py
+++ src/tests/varasto_test.py
@@ -13,10 +13,32 @@ class TestVarasto(unittest.TestCase):
     def test_uudella_varastolla_oikea_tilavuus(self):
         self.assertAlmostEqual(self.varasto.tilavuus, 10)
 
+    def test_uutta_varastoa_ei_voi_luoda_negatiivisella_tilavuudella(self):
+        varasto = Varasto(-10)
+        self.assertAlmostEqual(varasto.tilavuus, 0)
+
+    def test_varaston_str_palauttaa_oikean_merkkijonon(self):
+        self.assertEqual(str(self.varasto), "saldo = 0, vielä tilaa 10")
+    
+    def test_alku_saldo_ei_voi_olla_negatiivinen(self):
+        varasto = Varasto(10, -5)
+
+        self.assertAlmostEqual(varasto.saldo, 0)
+
     def test_lisays_lisaa_saldoa(self):
         self.varasto.lisaa_varastoon(8)
 
         self.assertAlmostEqual(self.varasto.saldo, 8)
+
+    def test_lisays_yli_saldon_lisaa_maksimikapasiteettiin(self):
+	    self.varasto.lisaa_varastoon(20)
+
+	    self.assertAlmostEqual(self.varasto.saldo, 10)
+
+    def test_negatiivisen_arvon_lisays_ei_muuta_saldoa(self):
+        self.varasto.lisaa_varastoon(-4)
+
+        self.assertAlmostEqual(self.varasto.saldo, 0)
 
     def test_lisays_lisaa_pienentaa_vapaata_tilaa(self):
         self.varasto.lisaa_varastoon(8)
@@ -30,6 +52,20 @@ class TestVarasto(unittest.TestCase):
         saatu_maara = self.varasto.ota_varastosta(2)
 
         self.assertAlmostEqual(saatu_maara, 2)
+
+    def test_negatiivisen_maaran_ottaminen_ei_tee_mitaan(self):
+        self.varasto.lisaa_varastoon(5)
+
+        saatu_maara = self.varasto.ota_varastosta(-2)
+
+        self.assertAlmostEqual(saatu_maara, 0)
+
+    def test_ottaminen_yli_saldon_ottaa_kaiken_mahdollisen_ei_enempää(self):
+        self.varasto.lisaa_varastoon(7)
+        
+        saatu_maara = self.varasto.ota_varastosta(20)
+
+        self.assertAlmostEqual(saatu_maara, 7)
 
     def test_ottaminen_lisaa_tilaa(self):
         self.varasto.lisaa_varastoon(8)
